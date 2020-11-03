b1e8e293fdd58159f2acaee4edf30bef6276ab7d
revelation <nusktecsoft@gmail.com>
2020-11-03 23:29:35 +0100

    Progress 70 - Online Database%

---
+++ res-codes.js
@@ -0,0 +1,26 @@
+/**
+ * Created by revelation on 03/11/2020.
+ * Reedax.IO Technologies Limited *
+ * Developer Revelation A.F *
+ */
+/*
+Error Codes
+
+2000 = Handshake to check if server is in a good state
+
+1000 = No valid content supplied, connection successful
+
+1001 = Invalid SSK or SSK not supplied (401 expected)
+
+1002 = Focused data is empty (428 expected)
+
+1004 = Url not found (404 expected)
+
+1005 = Invalid type of data supplied (416 expected)
+
+1006 = Valid data type supplied but mis-matched over comparism (417 expected)
+
+1007 = Expected data action already existed within internal data
+
+1008 = Related content length is not satisfied (416 expected). Case used in password length or a must fill data
+
